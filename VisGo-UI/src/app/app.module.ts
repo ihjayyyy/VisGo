7e44933f9d3d17146cf790b3ada82ee9f536214e
DanielMaceda <125423244+DanielMaceda@users.noreply.github.com>
2023-06-07 11:47:44 +0800

    updated the admin pages without error

--- VisGo-UI/src/app/app.module.ts
+++ VisGo-UI/src/app/app.module.ts
@@ -9,16 +9,11 @@ import { LoginComponent } from './auth/components/login/login.component';
 import { UserPagesModule } from './userpages/userpages.module';
 import { AdminDashboardComponent } from './userpages/admin-components/admin-dashboard/admin-dashboard.component';
 import { AdminLogbookComponent } from './userpages/admin-components/admin-logbook/admin-logbook.component';
-import { AdminDeleteReportComponent } from './userpages/admin-components/admin-delete-report/admin-delete-report.component';
-import { AdminDeleteUserComponent } from './userpages/admin-components/admin-delete-user/admin-delete-user.component';
 import { AdminDetailsOfVisitComponent } from './userpages/admin-components/admin-details-of-visit/admin-details-of-visit.component';
-import { AdminNewUserComponent } from './userpages/admin-components/admin-new-user/admin-new-user.component';
 import { AdminProfileComponent } from './userpages/admin-components/admin-profile/admin-profile.component';
 import { AdminReportDetailsComponent } from './userpages/admin-components/admin-report-details/admin-report-details.component';
-import { AdminReportsComponent } from './userpages/admin-components/admin-reports/admin-reports.component';
 import { AdminScheduledVisitDetailsComponent } from './userpages/admin-components/admin-scheduled-visit-details/admin-scheduled-visit-details.component';
 import { AdminScheduledVisitsComponent } from './userpages/admin-components/admin-scheduled-visits/admin-scheduled-visits.component';
-import { AdminSystemManagementComponent } from './userpages/admin-components/admin-system-management/admin-system-management.component';
 
 
 
@@ -31,16 +26,11 @@ import { AdminSystemManagementComponent } from './userpages/admin-components/adm
     RegisterComponent,
     AdminDashboardComponent,
     AdminLogbookComponent,
-    AdminDeleteReportComponent,
-    AdminDeleteUserComponent,
     AdminDetailsOfVisitComponent,
-    AdminNewUserComponent,
     AdminProfileComponent,
     AdminReportDetailsComponent,
-    AdminReportsComponent,
     AdminScheduledVisitDetailsComponent,
-    AdminScheduledVisitsComponent,
-    AdminSystemManagementComponent,
+    AdminScheduledVisitsComponent
     
     
   ],
